724fa48f103ebff75cfe1d476724a8e4c280355b
levyathan <sritharkalimuthu@gmail.com>
2024-02-08 17:29:12 +0530

    Frontend Challenge

--- Age-calculator/src/Calculator.js
+++ Age-calculator/src/Calculator.js
@@ -7,96 +7,101 @@ const UserInput = () => {
   const [month, setMonth] = useState('');
   const [year, setYear] = useState('');
   const [inputError, setInputError] = useState(false);
-  // const [dayerrorMessage,setDayerrorMessage] = useState('');
-  // const [montherrorMessage,setMontherrorMessage] = useState('');
-  // const [yearerrorMessage,setYearerrorMessage] = useState('');
+  const [errorMessage,seterrorMessage]=useState('');
   const [age, setAge] = useState({ years: '--', months: '--', days: '--' });
+  let Age_count;
 
   // Function to handle input changes
   const handleInputChange = (e) => {
     const { name, value } = e.target;
+    setInputError(false)
+    setAge({ years: '--', months: '--', days: '--' });
 
     // Validation and updating state based on input changes
 
     switch (name) {
       case 'day':
-        if (value === '' || value > 31 || value < 1) {
-          setInputError(true);
-        }else{
-          setInputError(false)
           setDay(value);
-        }
-        break
+          break
       case 'month':
-        if (value === '' || value > 12 || value < 1) {
-          setInputError(true);
-        }else{
-          setInputError(false)
-          setMonth(value);  
-        }
-        break
+          setMonth(value); 
+          break
       case 'year':
-        if (value === '' || value > 2200 || value < 1800) {
-          setInputError(true);
-        }else{
-          setInputError(false)
           setYear(value);
-        }
-        break
+          break
       default:
-        break
+          break
     }
   };
 
   // Function to calculate and display age
   const getuserInput = (e) => {
     e.preventDefault();
-
-    function calculation(){
-      const currentDate = new Date();
-
-            let ageYears =  currentDate.getFullYear() - year;
-            let ageMonths = currentDate.getMonth() - month+1;
-            let ageDays =Math.abs(currentDate.getDate() - day);
-
-            if(ageMonths<0){
-              ageMonths+=12;
-              ageYears-=1;
-            }
-            let daycount=0;
-            let monthcount=0;
-            let yearcount=0;
-
-            let Age_count=setInterval(()=>{
-
-              if(daycount<ageDays){
-                daycount+=1
-              }
-              if(monthcount<ageMonths){
-                monthcount+=1
-              }
-              if(yearcount<ageYears){
-                yearcount+=1
-              }
-                // Set the calculated age to state
-              setAge({
-                years: yearcount,
-                months: monthcount,
-                days: daycount,
-              })
-            },30);
-
-            setTimeout(()=>{
-              clearInterval(Age_count)
-            },4000);
-            }
+    const currentDate = new Date();
+    const userDate = new Date(`${year}-${month}-${day}`);
     
-    if( day==='' && month === '' && year===''){
+    if( day==='' || month === '' || year===''){
+      setInputError(true);
+      seterrorMessage('This Field is Required')
+    }else if( day>31||day<1 || month>12||month<1 || year>9999||year<1000 ){
       setInputError(true)
-    }else if (inputError===false){
+      seterrorMessage('Enter Valid Input')
+    }else if(year<1900){
+      setInputError(true)
+      seterrorMessage('Enter year above 1800')
+    }else if(year>currentDate.getFullYear()|| userDate>currentDate){
+      seterrorMessage('Must be in the Past')
+      setInputError(true)
+    }else if(inputError===false){
+      seterrorMessage('')
       setAge({ years: '--', months: '--', days: '--' });
       calculation();
-   };
+    };
+
+
+    function calculation(){
+      let ageYears =  currentDate.getFullYear() - year;
+      let ageMonths = currentDate.getMonth() - month+1;
+      let ageDays =currentDate.getDate() - day;
+
+      if(ageDays<0){
+        ageMonths-=1;
+        ageDays = currentDate.getDate();
+      }
+
+      if(ageMonths<0){
+        ageMonths+=12;
+        ageYears-=1;
+      }
+      let daycount=0;
+      let monthcount=0;
+      let yearcount=0;
+
+      Age_count=setInterval(()=>{
+
+        if(daycount<ageDays){
+          daycount+=1
+        }
+        if(monthcount<ageMonths){
+          monthcount+=1
+        }
+        if(yearcount<ageYears){
+          yearcount+=1
+        }
+        
+          // Set the calculated age to state
+        setAge({
+          years: yearcount,
+          months: monthcount,
+          days: daycount,
+        })
+      },30);
+
+      setTimeout(()=>{
+        clearInterval(Age_count)
+      },3500);
+      }
+
   }
 
   return (
@@ -116,7 +121,7 @@ const UserInput = () => {
                 name={fieldName}
                 onChange={handleInputChange}
               />
-              <p className={`error-text`}>{}</p>
+              <p className={`error-text`}>{errorMessage}</p>
             </div>
           ))}
         </section>
